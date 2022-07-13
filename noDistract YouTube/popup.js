1d19c58ca39f095b0db81d8c834226ca53432cf9
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-07-13 10:24:12 +0530

    first commit

---
+++ noDistract YouTube/popup.js
@@ -0,0 +1,45 @@
+const Sidebar = document.querySelector('#items > ytd-item-section-renderer');
+const Comments = document.querySelector('#comments');
+const Category = document.querySelector('#scroll-container');
+
+let isSidebar =1 , isCategory =1, isComments=1;
+
+document.querySelector('#hide-side').addEventListener('click', ()=>{
+    if(isSidebar === 1){
+        isSidebar = 0;        
+    }
+    else {isSidebar = 1}
+})
+document.querySelector('#hide-scroll').addEventListener('click', ()=>{
+    if(isCategory === 1){
+        isCategory = 0;        
+    }
+    else {isCategory = 1}
+})
+document.querySelector('#hide-comment').addEventListener('click', ()=>{
+    if(isComments === 1){
+        isComments = 0;        
+    }
+    else {isComments = 1}
+})
+
+
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{
+    if(tab.url === "https://youtube.com/"){
+        if(isSidebar === 0){
+            isSidebar.style.display = "none";
+        }
+        if(isCategory === 0){
+            isCategory.style.display = "none";
+        }
+        if(isComments === 0){
+            isComments.style.display = "none";
+        }
+    
+    }
+})
+
+
+
+document.getElementsByTagName('ytd-watch-next-secondary-results-renderer').style.visibility = "hidden";
